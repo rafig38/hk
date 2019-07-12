783b39426554a9088d3579617289b956ee789b93
rafig38 <rafig38@users.noreply.github.com>
2019-07-12 11:58:36 -0300

    Update QuartaLista-RubemAlvesFigueredo.hs

--- QuartaLista-RubemAlvesFigueredo.hs
+++ QuartaLista-RubemAlvesFigueredo.hs
@@ -1,11 +1,9 @@
 {- Dos slides "aula13-14 Gloss" os exercícios tapete de Sierpinsky e curva de Koch
  Todos os exercícios dos slides "aula15 polimorfismoETypeClasses"
 -}
-
---aula13-14 gloss
+--aulas-13e14 gloss
 -- TAPETE --
 module Main(main) where
-
 import Graphics.Gloss
 
 window :: Display
@@ -52,100 +50,96 @@ ckoch n [p,q] = pictures[ckoch (n-1) [p, p1]
                                                           
 main :: IO ()   
 main = display window white (ckoch 3 [(-100, 10),(300, 10)])
---aula15 polimorfismoETypeClasses
 
-{- 1º Considere a seguinte função:
+--aula-15 polimorfismoETypeClasses
 
+{- 1. Considere a seguinte função:
 shift ((x,y), z) = (x, (y, z))
 Qual é o seu tipo mais geral?
-
 R- shift :: ((a,b), c) -> (a, (b, c))
 -}
 shift :: ((a,b),c) -> (a, (b,c))
 shift ((x, y), z) = (x, (y, z))
 
-
-{-2º Considere a seguinte função:
-
+{-2. Considere a seguinte função:
 zip' [] ps = []
 zip' ps [] = []
 zip' (p:ps) (q:qs) = (p, q) : zip' ps qs
-
 O que ela calcula? Qual é seu tipo mais geral?
-
-R - A função zip' cria uma lista de tuplas com pares de elementos correspondentes
-    exmplo:
-    zip' [1..5] "rubem"
+R - A função zip' cria uma lista de tuplas contendo pares cujos primeiro elemento da primeira lista
+    com o primeiro elemento da segunda lista, o segundo elemento da primera lista com o segundo elemento
+    da segunda lista e assim por diante.	
+    Exemplo:
+    zip' [1,2,3,4,5] "rubem"
     >> [(1,'r'),(2,'u'),(3,'b'),(4,'e'),(5,'m')]
-
-   tipo mais geral [a] -> [b] -> [(a,b)]
-
+Tipo mais geral:
+ zip':: [a] -> [b] -> [(a,b)]
 -}
 zip' :: [a] -> [b] -> [(a,b)]
 zip' [] ps = []
 zip' ps [] = []
 zip' (p:ps) (q:qs) = (p, q) : zip' ps qs
 
-{-3º
-Defina uma função numEqual que pegue uma lista xs de items e
-um item x e retorne o número de vezes que x ocorre dentro de xs.
-Qual é o tipo da sua função? Como poderia usar numEqual para
-definir elem?
-
-R - A função é do tipo Eq a => a -> [a] -> Int
-    elem iria verificar a contagem de numEqual, digo,
-    se for maior ou igual a 1 será True, caso contrário retorna False.
-
-
+{-3.
+Defina uma função "numEqual" que pegue uma lista "xs" de items e
+um item "x" e retorne o número de vezes que "x" ocorre dentro de "xs".
+Qual é o tipo da sua função? Como poderia usar "numEqual" para
+definir "elem' "?
+R - A função é do tipo geral:
+    Eq a => a -> [a] -> Int
+    "elem" irá verificar a contagem de "numEqual".
+    Assim, se for maior ou igual a 1 será "True", caso contrário retorna "False".
 -}
-numEqual :: Eq a => a -> [a] -> Int -- A função permite qualquer tipo, desde que os tipos permitam a comparação (==).
+-- A função permite qualquer tipo, desde que os tipos permitam a comparação (==).
+numEqual :: Eq a => a -> [a] -> Int 
 numEqual _ [] = 0
 numEqual x (y:ys)
     | x == y = 1 + numEqual x ys
     | otherwise =  numEqual x ys
 
+-- Para ser "True" o elemento deve aparecer pelo menos uma vez.
 elem' :: Eq a => a -> [a] -> Bool
 elem' x xs
-    | (numEqual x xs) >= 1 = True -- Para ser True o elemento deve aparecer PELO MENOS UMA VEZ.
+    | (numEqual x xs) >= 1 = True 
     | otherwise            = False
 
-{-4º
-Defina a função oneLookupFirst que pega uma lista de pares e
-um item. Digamos que o tipo dos pares é (a, b), e que o tipo do
-item é a. A função retorna a segunda componente do primeiro par
+{-4.
+Defina a função "oneLookupFirst" que pega uma lista de pares e
+um item. Digamos que o tipo dos pares é "(a, b)" e que o tipo do
+item é "a". A função retorna a segunda componente do primeiro par
 cuja primeira componente é igual ao item. Qual é o tipo mais geral da
 função?
 R -
   Eq a => a -> [(a,b)] -> b
-
-Defina a função oneLookupSecond que retorna a primeira
+  
+Defina a função "oneLookupSecond" que retorna a primeira
 componente do primeiro par cuja segunda componente é igual ao
 item.
 Qual é o tipo mais geral da função?
 R -
   Eq b => b -> [(a,b)] -> a
 -}
+
 oneLookupFirst :: Eq a => a -> [(a,b)] -> b
-oneLookupFirst _ [] = error "Elemento não encontrado, ou lista vazia!"
+oneLookupFirst _ [] = error "Nothing have been found or empty list!"
 oneLookupFirst x (y:ys)
    | x == fst(y) = snd (y)
    | otherwise = oneLookupFirst x (ys)
 
 oneLookupSecond :: Eq b => b -> [(a,b)] -> a
-oneLookupSecond _ [] = error "Elemento não encontrado, ou lista vazia!"
+oneLookupSecond _ [] = error "Nothing have been found or empty list!"
 oneLookupSecond x (y:ys)
    | x == snd(y) = fst (y)
    | otherwise = oneLookupSecond x (ys)
 
-{-5º Considere a seguinte função
+{-5. Considere a seguinte função
 misterio y x = [ show z | z <- x, elem z y ]
 Qual é eu seu tipo mais geral?
-R -
-    misterio recebe duas listas. E retorna uma terceira lista com seus elementos convertidos
-    em string. A nova lista (z) irá receber apenas valores que está contido nas duas listas.
+R - misterio :: (Eq a, Show a) => [a] -> [a] -> [String]
 
-    tipo geral é misterio :: (Eq a, Show a) => [a] -> [a] -> [String]
-
+    "misterio" recebe duas listas e retorna uma terceira lista com seus elementos convertidos
+    em uma lista de string. A nova lista irá receber apenas valores que estão contidos nas duas listas.
+    Tipo geral é:  misterio :: (Eq a, Show a) => [a] -> [a] -> [String]
 -}
 misterio :: (Eq a, Show a) => [a] -> [a] -> [String]
 misterio y x = [ show z | z <- x, elem z y ]
