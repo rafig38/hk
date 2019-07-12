2a379363c78fbb896bdb80eac14341ac25cbb8b0
rafig38 <rafig38@users.noreply.github.com>
2019-07-11 23:31:50 -0300

    Add files via upload

--- QuartaLista-RubemAlvesFigueredo.hs
+++ QuartaLista-RubemAlvesFigueredo.hs
@@ -30,27 +30,28 @@ main :: IO ()
 main = display window background (tapete 4 (-150, -150) 200)
 
 -- Curva de Koch --
-odule Main(main) where
+module Main(main) where
 import Graphics.Gloss
+import qualified Graphics.Gloss.Data.Point.Arithmetic as V
+import Graphics.Gloss.Data.Vector
 
 window :: Display
 window = InWindow "Curva de Koch" (500, 500) (-20, -20)
 
-ckoch:: Int -> Point -> Point -> Picture
-ckoch 0 p q = line [p, q]
-ckoch n p q = pictures [ ckoch (n-1) p p1
-                         ,ckoch (n-1) p1 p2
-                         ,ckoch (n-1) p2 p3
-                         ,ckoch (n-1) p3 q ]                         
-               where v = mulSV (1/3) (q V.-p)
-                   p1 = p V.+v
-                   p2 = p1 V.+rotateV (45) v
-                   p3 = p2 V.+rotateV (-45) v
-                           
-
-main :: IO ()
-main = display window white (ckoch 4 360 360)
-
+ckoch:: Int -> Path-> Picture
+ckoch 0 ts = line ts
+ckoch n [p,q] = pictures[ckoch (n-1) [p, p1]
+                        , ckoch (n-1) [p1, p2]
+                        , ckoch (n-1) [p2, p3]
+                        , ckoch (n-1) [p3, q] ]                         
+                        where 
+                            v = mulSV (1/3) (q V.- p)
+                            p1 = p V.+ v
+                            p2 = p1 V.+ rotateV (pi/4) v
+                            p3 = p2 V.+ rotateV (-pi/4) v
+                                                          
+main :: IO ()   
+main = display window white (ckoch 3 [(-100, 10),(300, 10)])
 --aula15 polimorfismoETypeClasses
 
 {- 1º Considere a seguinte função:
