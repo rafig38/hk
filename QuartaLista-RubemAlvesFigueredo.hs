9e86af380b9799e569cc13e38ee2ac6444f56c40
rafig38 <rafig38@gmail.com>
2019-07-14 00:22:49 -0300

    alteracao

--- QuartaLista-RubemAlvesFigueredo.hs
+++ QuartaLista-RubemAlvesFigueredo.hs
@@ -19,11 +19,11 @@ tapete n (x, y) sz = pictures [ polygon [(x+sz, y+sz), (x+sz, y+2*sz), (x+2*sz, 
                   ,tapete (n-1) (x+sz, y) (sz/3)
                   ,tapete (n-1) (x+2*sz, y) (sz/3)
                   ,tapete (n-1) (x, y+sz) (sz/3)
-                  ,tapete (n-1) (x+2*sz, y+sz) (sz/3)  
-                  ,tapete (n-1) (x, y+2*sz) (sz/3) 
-                  ,tapete (n-1) (x+sz, y+2*sz) (sz/3) 
+                  ,tapete (n-1) (x+2*sz, y+sz) (sz/3)
+                  ,tapete (n-1) (x, y+2*sz) (sz/3)
+                  ,tapete (n-1) (x+sz, y+2*sz) (sz/3)
                   ,tapete (n-1) (x+2*sz, y+2*sz) (sz/3) ]
-                            
+
 main :: IO ()
 main = display window background (tapete 4 (-150, -150) 200)
 
@@ -41,14 +41,14 @@ ckoch 0 ts = line ts
 ckoch n [p,q] = pictures[ckoch (n-1) [p, p1]
                         , ckoch (n-1) [p1, p2]
                         , ckoch (n-1) [p2, p3]
-                        , ckoch (n-1) [p3, q] ]                         
-                        where 
+                        , ckoch (n-1) [p3, q] ]
+                        where
                             v = mulSV (1/3) (q V.- p)
                             p1 = p V.+ v
                             p2 = p1 V.+ rotateV (pi/4) v
                             p3 = p2 V.+ rotateV (-pi/4) v
-                                                          
-main :: IO ()   
+
+main :: IO ()
 main = display window white (ckoch 3 [(-100, 10),(300, 10)])
 
 --aula-15 polimorfismoETypeClasses
@@ -68,7 +68,7 @@ zip' (p:ps) (q:qs) = (p, q) : zip' ps qs
 O que ela calcula? Qual é seu tipo mais geral?
 R - A função zip' cria uma lista de tuplas contendo pares cujos primeiro elemento da primeira lista
     com o primeiro elemento da segunda lista, o segundo elemento da primera lista com o segundo elemento
-    da segunda lista e assim por diante.	
+    da segunda lista e assim por diante.
     Exemplo:
     zip' [1,2,3,4,5] "rubem"
     >> [(1,'r'),(2,'u'),(3,'b'),(4,'e'),(5,'m')]
@@ -91,7 +91,7 @@ R - A função é do tipo geral:
     Assim, se for maior ou igual a 1 será "True", caso contrário retorna "False".
 -}
 -- A função permite qualquer tipo, desde que os tipos permitam a comparação (==).
-numEqual :: Eq a => a -> [a] -> Int 
+numEqual :: Eq a => a -> [a] -> Int
 numEqual _ [] = 0
 numEqual x (y:ys)
     | x == y = 1 + numEqual x ys
@@ -100,7 +100,7 @@ numEqual x (y:ys)
 -- Para ser "True" o elemento deve aparecer pelo menos uma vez.
 elem' :: Eq a => a -> [a] -> Bool
 elem' x xs
-    | (numEqual x xs) >= 1 = True 
+    | (numEqual x xs) >= 1 = True
     | otherwise            = False
 
 {-4.
@@ -111,7 +111,7 @@ cuja primeira componente é igual ao item. Qual é o tipo mais geral da
 função?
 R -
   Eq a => a -> [(a,b)] -> b
-  
+
 Defina a função "oneLookupSecond" que retorna a primeira
 componente do primeiro par cuja segunda componente é igual ao
 item.
@@ -121,13 +121,13 @@ R -
 -}
 
 oneLookupFirst :: Eq a => a -> [(a,b)] -> b
-oneLookupFirst _ [] = error "Nothing have been found or empty list!"
+oneLookupFirst _ [] = error "empty list!"
 oneLookupFirst x (y:ys)
    | x == fst(y) = snd (y)
    | otherwise = oneLookupFirst x (ys)
 
 oneLookupSecond :: Eq b => b -> [(a,b)] -> a
-oneLookupSecond _ [] = error "Nothing have been found or empty list!"
+oneLookupSecond _ [] = error "empty list!"
 oneLookupSecond x (y:ys)
    | x == snd(y) = fst (y)
    | otherwise = oneLookupSecond x (ys)
